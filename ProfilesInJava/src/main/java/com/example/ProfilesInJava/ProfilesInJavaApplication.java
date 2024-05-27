0774b978fe60953369ace1f69c47ba2ab2504d97
D4_63263_Rakesh <rrpatil.9579@gmail.com>
2024-05-27 12:23:26 +0530

    Created Sample spring boot project

---
+++ ProfilesInJava/src/main/java/com/example/ProfilesInJava/ProfilesInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProfilesInJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesInJavaApplication.class, args);
+	}
+
+}
---
+++ ProfilesInJava/src/test/java/com/example/ProfilesInJava/ProfilesInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProfilesInJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
